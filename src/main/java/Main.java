1b47cc01153c6fbd1925d5381a68f0f2f3d32784
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-09-18 20:42:43 +0500

    Scratches for filesaver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,10 +6,9 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.lang.reflect.Type;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 public class Main {
     private static final String NASA_SOURCE = "https://api.nasa.gov/planetary/apod?api_key=QYjPmA3rmtTqnP5oYWd5K4b1hMPbvdGvahSTO5Q3";
@@ -25,15 +24,56 @@ public class Main {
                         .build())
                 .build();
         CloseableHttpResponse response = httpClient.execute(request);
-
-//        String body = new String(response.getEntity().getContent().readAllBytes(), StandardCharsets.UTF_8);
-//        System.out.println(body);
-
+        httpClient.close();
         Answer post = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
         });
+        fileSaver(post.getUrl());
+    }
 
-        System.out.println(post);
-        httpClient.close();
+//    public void saveUrl(final String filename, final String urlString)
+//            throws MalformedURLException, IOException {
+//        BufferedInputStream in = null;
+//        FileOutputStream fout = null;
+//        try {
+//            in = new BufferedInputStream(new URL(urlString).openStream());
+//            fout = new FileOutputStream(filename);
+//
+//            final byte data[] = new byte[1024];
+//            int count;
+//            while ((count = in.read(data, 0, 1024)) != -1) {
+//                fout.write(data, 0, count);
+//            }
+//        } finally {
+//            if (in != null) {
+//                in.close();
+//            }
+//            if (fout != null) {
+//                fout.close();
+//            }
+//        }
+//    }
+
+    private static void fileSaver(String url) {
+        if(url.isEmpty()) {
+            System.out.println("URL is empty: " + url);
+        } else {
+            String[] parts = url
+                    .split("/");
+            String fileName = parts[parts.length - 1];
+            // we alse need to download picture or video in a buffer?
+
+            File data = new File("C:\\Users\\user\\IdeaProjects\\Request_NASA\\" + fileName);
+            try {
+                if (data.createNewFile()) {
+                    FileWriter writer = new FileWriter(data);
+                    writer.write(json);
+                    writer.flush();
+                    writer.close();
+                }
+            } catch (IOException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
     }
 
 }
