cd796023c90946f4edff6aaff9dd6d9a1601f2a5
SimonIsNotAvailable <polushkinsn@gmail.com>
2022-09-19 10:32:13 +0500

    fileSaver working and NasaRequest method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,15 +7,26 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
+import java.io.BufferedOutputStream;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
 
 public class Main {
     private static final String NASA_SOURCE = "https://api.nasa.gov/planetary/apod?api_key=QYjPmA3rmtTqnP5oYWd5K4b1hMPbvdGvahSTO5Q3";
     public static final ObjectMapper mapper = new ObjectMapper();
 
     public static void main(String[] args) throws IOException {
-        HttpGet request = new HttpGet(NASA_SOURCE);
+        Answer post = mapper.readValue(nasaRequest(NASA_SOURCE)
+                .getEntity()
+                .getContent(), new TypeReference<>() {
+        });
+        System.out.println(post);
+        fileSaver(post.getUrl(), post);
+    }
+
+    private static CloseableHttpResponse nasaRequest(String url) throws IOException {
+        HttpGet request = new HttpGet(url);
         CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(RequestConfig.custom()
                         .setConnectTimeout(5000)
@@ -24,56 +35,32 @@ public class Main {
                         .build())
                 .build();
         CloseableHttpResponse response = httpClient.execute(request);
-        httpClient.close();
-        Answer post = mapper.readValue(response.getEntity().getContent(), new TypeReference<>() {
-        });
-        fileSaver(post.getUrl());
+        return response;
     }
 
-//    public void saveUrl(final String filename, final String urlString)
-//            throws MalformedURLException, IOException {
-//        BufferedInputStream in = null;
-//        FileOutputStream fout = null;
-//        try {
-//            in = new BufferedInputStream(new URL(urlString).openStream());
-//            fout = new FileOutputStream(filename);
-//
-//            final byte data[] = new byte[1024];
-//            int count;
-//            while ((count = in.read(data, 0, 1024)) != -1) {
-//                fout.write(data, 0, count);
-//            }
-//        } finally {
-//            if (in != null) {
-//                in.close();
-//            }
-//            if (fout != null) {
-//                fout.close();
-//            }
-//        }
-//    }
 
-    private static void fileSaver(String url) {
-        if(url.isEmpty()) {
+    private static void fileSaver(String url, Answer post) throws IOException {
+        if (url.isEmpty()) {
             System.out.println("URL is empty: " + url);
         } else {
             String[] parts = url
                     .split("/");
             String fileName = parts[parts.length - 1];
-            // we alse need to download picture or video in a buffer?
-
-            File data = new File("C:\\Users\\user\\IdeaProjects\\Request_NASA\\" + fileName);
+            byte[] bytes = nasaRequest(url).getEntity().getContent().readAllBytes();
+            File file = new File("C:\\Users\\user\\IdeaProjects\\Request_NASA\\resources\\" + fileName);
             try {
-                if (data.createNewFile()) {
-                    FileWriter writer = new FileWriter(data);
-                    writer.write(json);
-                    writer.flush();
-                    writer.close();
+                if (file.createNewFile()) {
+                    try (OutputStream outputStream =
+                                 new BufferedOutputStream(new FileOutputStream(file))) {
+                        outputStream.write(bytes);
+                         System.out.println(post.getTitle() + "has been saved");
+                    }
+                } else if (file.exists()){
+                    System.out.println("File \"" + fileName +"\" already exists in the directory");
                 }
             } catch (IOException ex) {
                 System.out.println(ex.getMessage());
             }
         }
     }
-
 }
